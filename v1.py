960e8599854b4ac7d8dd92c129faad9757b75b0a
karthikskumar <karthiksk.blr@gmail.com>
2018-09-12 07:20:08 +0530

    Commented the shit out

--- v1.py
+++ v1.py
@@ -8,12 +8,18 @@ import json
 # Function accepts a string of location
 def getLatLng(loc, url = 'https://maps.google.com/maps/api/geocode/json?address='):
 
+    # Replace any spaces in the location name with '%20'
     loc  = loc.replace(" ","%20")
     pageURL = url + loc
+
+    # Open the URL
     page = urlopen(pageURL)
     html = page.read()
 
+    # Read page as json
     latlng = json.loads(html)
+
+    # If page is empty (Reached google api limit) return empty list.
     if latlng['results'] == []:
         return(None)
     lat = latlng['results'][0]['geometry']['location']['lat']
@@ -32,6 +38,7 @@ def getDarkSky(loc = None, unit = 'ca12', url='https://darksky.net/forecast/'):
         'uk212' = Celsius and mi/h
     '''
 
+    # Use current location if no location is provided
     if loc is None or loc == '':
         print('Using current location from IP Address')
         location = getLoc()
@@ -45,24 +52,33 @@ def getDarkSky(loc = None, unit = 'ca12', url='https://darksky.net/forecast/'):
         lat = str(location['lat'])
         lng = str(location['lng'])
 
+    # Use lat and long in URL
     pageURL = url + lat + ',' + lng + '/' + unit
+
+    # Open page
     page = urlopen(pageURL)
     html = page.read()
 
+    # Soupify
     soup = bs(html.decode(), 'html.parser')
 
+    # Empty weather dict to hold values
     weather = {}
 
+    # Temperature (not temporary)
     temp = soup.find("span", {"class": "summary swap" })
     temp = temp.getText()
     temp = temp.replace('\xa0',' and ')
 
     weather['Temperature'] = temp
 
+    # Other details
+    # This is separate because of the DarkSky page structure
     dets = soup.find("div", {"id": "currentDetails"})
     det = dets.findChild()
 
-
+    # Every Child of "currentDetails" has a name and value child
+    # The name is the weather parameter and the num is the value
     while det is not None:
 
         namVal = det.text
@@ -78,7 +94,8 @@ def getDarkSky(loc = None, unit = 'ca12', url='https://darksky.net/forecast/'):
     return weather
 
 
-# Function to get Location
+# Function to get Location (uses system's IP address)
+# This can be inaccurate networks that use NATs
 def getLoc():
 
     url = 'http://ip-api.com/json'
@@ -88,6 +105,8 @@ def getLoc():
     loc = json.loads(html)
     return(loc)
 
+# Function to tell if it is getting dark outside
+# Uses UV values as proxy for brightness.
 def isDark(loc = None, threshold = 0):
     weather = getDarkSky(loc)
     if weather is None:
